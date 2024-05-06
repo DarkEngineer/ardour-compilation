266d4dd9c33fc384b801565366f4d0e58a58ef3c
Dark Engineer <maniodark@gmail.com>
2024-05-06 21:21:29 +0200

    Update ardour-win.sh

--- ardour-win.sh
+++ ardour-win.sh
@@ -1,6 +1,7 @@
 sudo apt install g++-mingw-w64-i686-win32 \
   build-essentials \
   libboost-all-dev \
-  gcc-mingw-w64
+  gcc-mingw-w64 \
+  pkg-config
 
 sudo ln -s /usr/bin/x86_64-w64-mingw32-windres /usr/bin/windres
