9b10c7d25a50fc50118f172f6c3398a959fa4bdb
Dark Engineer <maniodark@gmail.com>
2024-05-06 21:25:06 +0200

    Update ardour-win.sh

--- ardour-win.sh
+++ ardour-win.sh
@@ -6,6 +6,7 @@ sudo apt install g++-mingw-w64-i686-win32 \
   libglibmm-2.4-dev \
   libsndfile1-dev \
   libcurl4-gnutls-dev \
-  libarchive-dev
+  libarchive-dev \
+  liblo-dev
 
 sudo ln -s /usr/bin/x86_64-w64-mingw32-windres /usr/bin/windres
